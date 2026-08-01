b6e376dfc9c8d0c5e4b1c700a96fefcefb4a1ccc
afavian <130101433+afavian@users.noreply.github.com>
2026-07-31 19:11:33 -0700

    Update theme.js

--- js/theme.js
+++ js/theme.js
@@ -1,1 +1,43 @@
-
+const picker = document.querySelector("#theme-picker");
+const radios = document.querySelectorAll(
+  "input[name='theme']"
+);
+function applyTheme(theme){
+  if(theme === "auto"){
+    document.documentElement.removeAttribute(
+      "data-theme"
+    );
+  }
+  else{
+    document.documentElement.dataset.theme = theme;
+  }
+}
+try{
+  const saved = localStorage.getItem("theme");
+  if(saved){
+    applyTheme(saved);
+    document.querySelector(
+      `input[value="${saved}"]`
+    ).checked = true;
+  }
+}
+catch(error){
+  
+}
+radios.forEach(radio => {
+  radio.addEventListener(
+    "change",
+    (event)=>{
+      const theme = event.target.value;
+      applyTheme(theme);
+      try{
+        localStorage.setItem(
+          "theme",
+          theme
+        );
+      }
+      catch(error){
+        
+      }
+    });
+});
