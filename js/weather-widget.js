50a939ffaa92987d1559a5d472fc6430b6621df1
afavian <130101433+afavian@users.noreply.github.com>
2026-07-31 20:30:24 -0700

    Update weather-widget.js

--- js/weather-widget.js
+++ js/weather-widget.js
@@ -18,7 +18,16 @@ class WeatherWidget extends HTMLElement {
     }
   }
   async loadWeather(){
-    
+    this.setAttribute("state", "loading");
+    const template = document.querySelector("#weather-template");
+    const clone = template.content.cloneNode(true);
+    const title = clone.querySelector(".weather-title");
+    const description = clone.querySelector(".weather-description");
+    const temperature = clone.querySelector(".weather-temperature");
+    title.textContent = 'Weather for ${this.getAttribute("location")};
+    description.textContent = "Loading...";
+    temperature.textContent = "";
+    this.replaceChildren(clone);
   }
 }
 customElements.define(
